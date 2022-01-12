8f7be3975a250704f83fdd2f296246d6dd276486
Camila Melo <dantas.mill.melo@gmail.com>
2022-01-12 13:58:53 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -6,7 +6,19 @@ function Listing() {
       <Pagination />
       <div className="container">
         <div className="row">
-          <div className="col">
+          <div className="col-sm-6 col-lg-4 col-x1-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-x1-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-x1-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-x1-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-x1-3 mb-3">
             <MovieCard />
           </div>
         </div>
